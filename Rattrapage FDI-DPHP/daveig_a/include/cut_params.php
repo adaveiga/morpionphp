706e39e2eabaec6e8374776acd17535ad311234b
cadet_a <cadet_a@c001a5dc-17f2-4cee-bf1a-a34fb5d02604>
2014-12-16 13:15:02 +0000

    Debut : entrer standar -> main.php & menu + exit -> include

---
+++ Rattrapage FDI-DPHP/daveig_a/include/cut_params.php
@@ -0,0 +1,14 @@
+<?php
+// cut_params.php for  in /home/cadet_a/Rattrapage/PHP
+// 
+// Made by CADET Anthony
+// Login   <cadet_a@etna-alternance.net>
+// 
+// Started on  Tue Dec 16 13:30:16 2014 CADET Anthony
+// Last update Tue Dec 16 13:31:24 2014 CADET Anthony
+//
+
+function	cut_params($line)
+{
+  return (explode(' ', $line));
+}
---
+++ Rattrapage FDI-DPHP/daveig_a/include/menu.php
@@ -0,0 +1,19 @@
+<?php
+// menu.php for  in /home/cadet_a/Rattrapage/PHP/include
+// 
+// Made by CADET Anthony
+// Login   <cadet_a@etna-alternance.net>
+// 
+// Started on  Tue Dec 16 13:11:17 2014 CADET Anthony
+// Last update Tue Dec 16 14:12:16 2014 CADET Anthony
+//
+
+function	f_menu()
+{
+  passthru("clear");
+  echo "*** Menu ***\n\n";
+  echo "1 : Partie solo\n";
+  echo "2 : 1 vs 1\n";
+  echo "0 : exit\n\n";
+  echo "Choix : ";
+}
