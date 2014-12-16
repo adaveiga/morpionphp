7d0dcc1dc07d65de80531629bb07dcaa664182cf
cadet_a <cadet_a@c001a5dc-17f2-4cee-bf1a-a34fb5d02604>
2014-12-16 15:08:15 +0000

    1v1 -> underscor

--- include/menu.php
+++ include/menu.php
@@ -5,15 +5,15 @@
 // Login   <cadet_a@etna-alternance.net>
 // 
 // Started on  Tue Dec 16 13:11:17 2014 CADET Anthony
-// Last update Tue Dec 16 14:12:16 2014 CADET Anthony
+// Last update Tue Dec 16 14:32:04 2014 CADET Anthony
 //
 
 function	f_menu()
 {
   passthru("clear");
   echo "*** Menu ***\n\n";
-  echo "1 : Partie solo\n";
-  echo "2 : 1 vs 1\n";
-  echo "0 : exit\n\n";
+  echo "1 ~> Partie solo\n";
+  echo "2 ~> 1 vs 1\n";
+  echo "0 ~> Quitter\n\n";
   echo "Choix : ";
 }
